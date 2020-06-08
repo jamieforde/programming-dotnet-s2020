897bf1de0e32e1a04bc27dad31371660653fafb3
Jamie Forde <000381120@mohawkcollege.ca>
2020-06-08 19:32:11 -0400

    WIP - Need to add some comments but Collections project Week3 starter code has been included.

--- Week3_Collections/Program.cs
+++ Week3_Collections/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,59 @@ namespace Week3_Collections
     {
         static void Main(string[] args)
         {
+            var names = new List<string>(100) { "Frodo", "Gandalf", "Bilbo", "Gimli" };
+            
+            Console.WriteLine($"List Count:  {names.Count}");
+            Console.WriteLine($"List Capacity: {names.Capacity}");
+
+            names.Add("Samwise");
+            DisplayNames(names);
+
+            names.Insert(0, "Gollum");
+            DisplayNames(names);
+
+            names.Remove("Gimli");
+            DisplayNames(names);
+
+            names.RemoveAt(0);
+            DisplayNames(names);
+
+
+            Console.WriteLine($"Does the List have Gimli: {names.Contains("Gimli")}");
+
+            names.TrimExcess();
+            Console.WriteLine($"List Capacity: {names.Capacity}");
+
+            var namesOrdered =
+                from name in names
+                orderby name descending
+                select name;
+
+            var filterNames =
+                from name in names
+                let upperName = name.ToUpper()
+                where upperName.StartsWith("G")
+                select upperName;
+
+            foreach(var name in filterNames)
+            {
+                Console.WriteLine(name);
+            }
+
+
+            //foreach (var name in namesOrdered)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+
+            Console.ReadKey();
+       
+        }
+
+        public static void DisplayNames(List<string> names)
+        {
+                 Console.WriteLine(string.Join(",", names));
         }
     }
 }
