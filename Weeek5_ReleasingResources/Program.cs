d5a70a51efe2ac2e46897663ef016d73889a00cd
Jamie Forde <000381120@mohawkcollege.ca>
2020-07-11 12:17:57 -0400

    Added projects for exception handling

---
+++ Weeek5_ReleasingResources/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weeek5_ReleasingResources
+{
+    class Program
+    {
+        /* Programs frequently request resoucres dynamically (at runtime)
+         * We explored this in File I/O section when working with files
+         * It best practice to release such resources when we have finished working with them 
+         * Not releasing resource such as file can cause resource leaks
+         * C# exception handling mechanism provides the finally block to allow resources to be
+         * released when an try block succeeds or fails (throw an exception)
+         * A finally block is always executed
+         
+         * In this example we are attempting to read in a file
+         * We can investigate what exceptions a StreamReader may generate using
+         * the Object Browser.  Access View-> Object Browser from file menu
+        */
+
+        static void Main(string[] args)
+        {
+            StreamReader streamReader = null;
+            var continueLoop = true;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Enter a file path:");
+                    var path = Console.ReadLine();
+                    streamReader = new StreamReader(path);
+                    Console.WriteLine("StreanReader was successfully executed!");
+                    continueLoop = false;
+                }
+                catch (ArgumentException argumentException)
+                {
+                    Console.WriteLine($"{argumentException.Message}");
+                    Console.WriteLine("The path cannot be empty string");
+                }
+                catch (IOException ioException)
+                {
+                    Console.WriteLine($"{ioException.Message}");
+                    Console.WriteLine($"File or Directory not found");
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"{ exception.Message}");
+                    Console.WriteLine("General Error");
+                }
+
+                // This block is always executed if an exception occurs or not.  
+                finally
+                {
+                    Console.WriteLine($"Finally always executes");
+
+                    // NOTE:   The null conditional operator ?.
+                    // If an exception is thrown at line 32 the streamReader is never instatiated.
+                    // That is it is null
+                    // So if we do not check streamReader has a reference this will through 
+                    // an exception.  
+                    // We could check with:
+                    // if (streamReader != null) - the null conditional operator is shortcut. 
+                    // and I think more readable
+
+                    streamReader?.Close();
+                }
+            } while (continueLoop);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Week5_ExceptionProperties/Program.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Exception classes are derived from the class Exception -> This is a great example of Inheritance
+ * Exception Properties:
+    * Message:  Stores a string error message for the exception object.
+    * StackTrace:  A string representation of the method-call stack
+    * InnerException:  Is an exception that caused the current exception.  
+        It is conveinent way to copy the details of an exception.  
+    * HelpLink:  Specifies a link to the help file that describes the problem
+    * Source:  Specifies the assembly that caused the exception
+    * TargetSite: specifies the method where the exception originated
+*/
+
+namespace Week5_ExceptionProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Method1();
+            } catch (Exception exceptionParameter)
+            {
+                Console.WriteLine($"exceptionParameter.ToString():\n\t {exceptionParameter}");
+                Console.WriteLine($"exceptionParameter.Message:\n\t {exceptionParameter.Message}");
+                Console.WriteLine($"exceptionParameter.StackTrace:\n\t {exceptionParameter.StackTrace}");
+                Console.WriteLine($"exceptionParameter.InnerException:\n\t {exceptionParameter.InnerException}");               
+            } finally
+            {
+                Console.WriteLine("Remember the finally always executes.");
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Method1() =>  Method2();
+            
+        static void Method2()
+        {
+            Method3();
+        }
+
+        static void Method3()
+        {
+            try
+            {
+                int.Parse("This is not an integer");
+            }
+            catch (FormatException formatException)
+            {
+                throw new Exception("Exception Occured in Method 3", formatException);
+            }
+        }
+    }
+}
